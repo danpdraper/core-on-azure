6f0519baa2a09a70791d2cf7427dd3487b14b0ab
Daniel Draper <daniel.draper@canonical.com>
2025-02-09 13:10:30 -0800

    feat: Add linux-azure kernel and convert disk to image gallery image

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 To build the raw image, run the following from this repository's root:
 
 ```
-$ SNAPCRAFT_KEY_NAME=<name of GPG key registered with Snapcraft> GADGET_SNAP_PATH=<path to local Azure gadget snap> SNAPCRAFT_STORE_CREDENTIALS=$(cat <path to Snapcraft credentials file>) make image
+$ SNAPCRAFT_KEY_NAME=<name of GPG key registered with Snapcraft> GADGET_SNAP_PATH=<path to local Azure gadget snap> KERNEL_SNAP_PATH=<path to local Azure kernel snap> SNAPCRAFT_STORE_CREDENTIALS=$(cat <path to Snapcraft credentials file>) make raw_image
 ```
 
-For instructions regarding creation and registration of a GPG key for Ubuntu Core model signing (the name of which you assign to the `SNAPCRAFT_KEY_NAME` variable in the preceding command), see https://ubuntu.com/core/docs/sign-model-assertion. To build a local Azure gadget snap, see https://github.com/danpdraper/pc-gadget/tree/azure. To obtain your Snapcraft credentials (in the interest of assigning those credentials to the `SNAPCRAFT_STORE_CREDENTIALS` variable in the preceding command), see https://ubuntu.com/core/docs/create-ubuntu-one#heading--snapcraft-credentials.
+For instructions regarding creation and registration of a GPG key for Ubuntu Core model signing (the name of which you assign to the `SNAPCRAFT_KEY_NAME` variable in the preceding command), see https://ubuntu.com/core/docs/sign-model-assertion. To build a local Azure gadget snap, see https://github.com/danpdraper/pc-gadget/tree/azure. To build a local Azure kernel snap, see https://git.launchpad.net/~ckt-core-cloud-test/+git/azure-kernel-snaps-uc24. To obtain your Snapcraft credentials (in the interest of assigning those credentials to the `SNAPCRAFT_STORE_CREDENTIALS` variable in the preceding command), see https://ubuntu.com/core/docs/create-ubuntu-one#heading--snapcraft-credentials.
 
-To convert the raw image to a fixed-size VHD, and then publish that VHD to Azure, run the following from this repository's root:
+To convert the raw image to a fixed-size VHD, publish that VHD to Azure, and create an image gallery image version from that VHD, run the following from this repository's root:
 
 ```
-$ AZURE_RESOURCE_GROUP_NAME=<name of target resource group for disk> AZURE_DISK_NAME=<name of disk> make disk
+$ AZURE_RESOURCE_GROUP_NAME=<name of target resource group for disk> AZURE_DISK_NAME=<name of disk> make image_gallery_image_version
 ```
--- functions.sh
+++ functions.sh
@@ -8,8 +8,8 @@ function create_raw_image() {
 		return 1
 	fi
 
-	if [ "$#" -ne 4 ] ; then
-		echo_error "USAGE: ${FUNCNAME[0]} snapcraft_key_name unsigned_model_path signed_model_path gadget_snap_path"
+	if [ "$#" -ne 5 ] ; then
+		echo_error "USAGE: ${FUNCNAME[0]} snapcraft_key_name unsigned_model_path signed_model_path gadget_snap_path kernel_snap_path"
 		echo_error "PROVIDED: ${FUNCNAME[0]} $*"
 		return 1
 	fi
@@ -17,9 +17,10 @@ function create_raw_image() {
 	local unsigned_model_path="$2"
 	local signed_model_path="$3"
 	local gadget_snap_path="$4"
+	local kernel_snap_path="$5"
 
 	snap sign -k $snapcraft_key_name $unsigned_model_path > $signed_model_path || return $?
-	ubuntu-image snap --snap $gadget_snap_path $signed_model_path || return $?
+	ubuntu-image snap --snap $gadget_snap_path --snap $kernel_snap_path $signed_model_path || return $?
 }
 
 BYTES_PER_MEGABYTE=$(( 1024 * 1024 ))
@@ -45,20 +46,22 @@ function create_disk_from_raw_image() {
 }
 
 function publish_disk() {
-	if [ "$#" -ne 3 ] ; then
-		echo_error "USAGE: ${FUNCNAME[0]} resource_group_name disk_name local_disk_path"
+	if [ "$#" -ne 4 ] ; then
+		echo_error "USAGE: ${FUNCNAME[0]} resource_group_name disk_name timestamp_in_seconds local_disk_path"
 		echo_error "PROVIDED: ${FUNCNAME[0]} $*"
 		return 1
 	fi
 	local resource_group_name="$1"
 	local disk_name="$2"
-	local local_disk_path="$3"
+	local timestamp_in_seconds="$3"
+	local local_disk_path="$4"
 
 	az group create --location westeurope --name $resource_group_name || return $?
 
+	local full_disk_name=$(echo ${disk_name}-$(date --date=@$timestamp_in_seconds +'%Y%m%d%H%M%S'))
 	local disk_size=$(stat --format '%s' $local_disk_path)
 	az disk create \
-		--name $disk_name \
+		--name $full_disk_name \
 		--resource-group $resource_group_name \
 		--location westeurope \
 		--zone 3 \
@@ -70,7 +73,7 @@ function publish_disk() {
 
 	local disk_access_sas_url=$(
 		az disk grant-access \
-			--name $disk_name \
+			--name $full_disk_name \
 			--resource-group $resource_group_name \
 			--access-level Write \
 			--duration-in-seconds 86400 | jq --raw-output .accessSas)
@@ -78,11 +81,43 @@ function publish_disk() {
 	azcopy copy $local_disk_path $disk_access_sas_url --blob-type PageBlob || return $?
 
 	az disk revoke-access \
-		--name $disk_name \
+		--name $full_disk_name \
 		--resource-group $resource_group_name || return $?
 }
 
-function create_and_publish_disk() {
+function create_image_version_from_disk() {
+	if [ "$#" -ne 3 ] ; then
+		echo_error "USAGE: ${FUNCNAME[0]} resource_group_name disk_name timestamp_in_seconds"
+		echo_error "PROVIDED: ${FUNCNAME[0]} $*"
+		return 1
+	fi
+	local resource_group_name="$1"
+	local disk_name="$2"
+	local timestamp_in_seconds="$3"
+
+	az sig create \
+		--resource-group $resource_group_name \
+		--gallery-name ${resource_group_name}_image_gallery
+
+	az sig image-definition create \
+		--resource-group $resource_group_name \
+		--gallery-name ${resource_group_name}_image_gallery \
+		--gallery-image-definition ${resource_group_name}-image-definition \
+		--publisher core-on-azure \
+		--offer core-on-azure \
+		--sku core-on-azure \
+		--os-type Linux \
+		--os-state generalized
+
+	az sig image-version create \
+		--resource-group $resource_group_name \
+		--gallery-name ${resource_group_name}_image_gallery \
+		--gallery-image-definition ${resource_group_name}-image-definition \
+		--gallery-image-version $(date --date=@$timestamp_in_seconds +'%Y.%m.%d%H%M%S') \
+		--os-snapshot $(az disk show --name $disk_name --resource-group $resource_group_name --query "id" | tr -d '"')
+}
+
+function create_image_gallery_image_version() {
 	if [ "$#" -ne 4 ] ; then
 		echo_error "USAGE: ${FUNCNAME[0]} raw_image_path local_disk_path resource_group_name disk_name"
 		echo_error "PROVIDED: ${FUNCNAME[0]} $*"
@@ -93,8 +128,11 @@ function create_and_publish_disk() {
 	local resource_group_name="$3"
 	local disk_name="$4"
 
+	local timestamp_in_seconds=$(date +'%s')
+
 	create_disk_from_raw_image $raw_image_path $local_disk_path || return $?
-	publish_disk $resource_group_name $disk_name $local_disk_path || return $?
+	publish_disk $resource_group_name $disk_name $timestamp_in_seconds $local_disk_path || return $?
+	create_image_version_from_disk $resource_group_name $disk_name $timestamp_in_seconds || return $?
 }
 
 
